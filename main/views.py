fba869d5066cd819bdc2ec3e661fe5e85992a278
Carl <alfelor14@gmail.com>
2025-08-02 20:25:48 +0800

    Add product page, styles, and js

--- main/views.py
+++ main/views.py
@@ -4,3 +4,7 @@ def home(request):
     print(f"[DEBUG] request.user: {request.user}")
     print(f"[DEBUG] Is authenticated: {request.user.is_authenticated}")
     return render(request, 'main/home.html')
+
+def product(request):
+    return render(request, 'main/product.html')
+
